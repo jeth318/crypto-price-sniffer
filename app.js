b43619020b6fcefe1f101224cc15bf4ac9016a94
jeth318 <jesper.thornberg@me.com>
2021-12-13 02:06:55 +0100

    More env

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
-require('dotenv').config();
-const { Spot } = require('@binance/connector')
-const { sha256 } = require('js-sha256');
-const apiKey = process.env.BINANCE_API_KEY
-const apiSecret = process.env.BINANCE_API_SECRET
-const client = new Spot(apiKey, apiSecret)
-const { cryptoSymbol } = require('crypto-symbol');
-const coinList = require('./complete-coinlist.json');
-const getPriceData = require('./price-updater');
+require("dotenv").config();
+const { Spot } = require("@binance/connector");
+const { sha256 } = require("js-sha256");
+const apiKey = process.env.BINANCE_API_KEY;
+const apiSecret = process.env.BINANCE_API_SECRET;
+const client = new Spot(apiKey, apiSecret);
+const { cryptoSymbol } = require("crypto-symbol");
+const coinList = require("./complete-coinlist.json");
+const priceUpdater = require("./price-updater");
 
 const { nameLookup } = cryptoSymbol({});
 const getGeckoIdFromSymbol = (symbol) => {
-    const matches = coinList.filter(c => c.symbol.toUpperCase() === symbol);
-    return matches.length ? matches[0].id : null;
-}
+  const matches = coinList.filter((c) => c.symbol.toUpperCase() === symbol);
+  return matches.length ? matches[0].id : null;
+};
 
-const getCoinNames = assets => assets.map(coin => ({
+const getCoinNames = (assets) =>
+  assets.map((coin) => ({
     name: nameLookup(coin.asset),
     symbol: coin.asset,
-    geckoId: getGeckoIdFromSymbol(coin.asset)
-}));
+    geckoId: getGeckoIdFromSymbol(coin.asset),
+  }));
 
-let previousHash = '';
+let previousHash = "";
 
 setInterval(() => {
-    client.account()
-        .then(async response => {
-            const assets = response?.data?.balances?.filter(asset => parseFloat(asset.free) > 0 || parseFloat(asset.locked) > 0);
-            const newHash = sha256(JSON.stringify(assets));
+  client
+    .account()
+    .then(async (response) => {
+      const assets = response?.data?.balances?.filter(
+        (asset) => parseFloat(asset.free) > 0 || parseFloat(asset.locked) > 0
+      );
+      const newHash = sha256(JSON.stringify(assets));
 
-            if (!previousHash) {
-                previousHash = newHash;
-            } else if (previousHash === newHash) {
-                const geckoIds = getCoinNames(assets).map(c => c.geckoId).filter(id => !!id);
-                // Go ahead and update prices
-                getPriceData(geckoIds);
-                // Override the previous hash value with the new one
-                previousHash = newHash;
-            }
-        })
-        .catch(error => console.log('ERROR', error))
+      if (!previousHash) {
+        previousHash = newHash;
+      } else if (previousHash === newHash) {
+        const geckoIds = getCoinNames(assets)
+          .map((c) => c.geckoId)
+          .filter((id) => !!id);
+        // Go ahead and update prices
+        priceUpdater(geckoIds);
+        // Override the previous hash value with the new one
+        previousHash = newHash;
+      }
+    })
+    .catch((error) => console.log("Error", error));
 }, 3000);
--- price-updater.js
+++ price-updater.js
@@ -1,55 +1,53 @@
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
 const { MongoClient } = require("mongodb");
 const axios = require("axios");
-const { sha256 } = require("js-sha256");
-const { DB_USERNAME, DB_PASSWORD } = process.env;
-const coinList = require('./complete-coinlist.json');
+const { DB_USERNAME, DB_PASSWORD, MONGODB_ENDPOINT } = process.env;
+const coinList = require("./complete-coinlist.json");
 
 // Connection URL
-const url =
-    `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.d9tv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${MONGODB_ENDPOINT}`;
+console.log(url);
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = "crypto-prices";
 
-// Interval (15 minutes)
-const time = 60 * 1000 * 1;
-
 const insertStuff = async (collection, stuff) =>
-    await collection.insertMany(stuff);
+  await collection.insertMany(stuff);
 
 const getGeckoSymbolFromGeckoId = (geckoId) => {
-    const matches = coinList.filter(c => c.id.toUpperCase() === geckoId.toUpperCase());
-    return matches.length ? matches[0].symbol : null;
-}
+  const matches = coinList.filter(
+    (c) => c.id.toUpperCase() === geckoId.toUpperCase()
+  );
+  return matches.length ? matches[0].symbol : null;
+};
 const getPriceData = async (ids) => {
-    const encodedIds = encodeURIComponent(ids.toString())
-    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${encodedIds}&vs_currencies=sek%2Cusd&include_last_updated_at=true`
-    const { data } = await axios(url);
-    const result = Object.keys(data).map((key) => ({
-        id: key,
-        symbol: getGeckoSymbolFromGeckoId(key),
-        last_updated_at: data[key].last_updated_at,
-        sek: data[key].sek,
-        usd: data[key].usd,
-        date: new Date(data[key].last_updated_at * 1000).toLocaleString(),
-    }));
+  const encodedIds = encodeURIComponent(ids.toString());
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${encodedIds}&vs_currencies=sek%2Cusd&include_last_updated_at=true`;
+  const { data } = await axios(url);
+  const result = Object.keys(data).map((key) => ({
+    id: key,
+    symbol: getGeckoSymbolFromGeckoId(key),
+    last_updated_at: data[key].last_updated_at,
+    sek: data[key].sek,
+    usd: data[key].usd,
+    date: new Date(data[key].last_updated_at * 1000).toLocaleString(),
+  }));
 
-    updateDb(result)
-    return result;
-}
-async function updateDb(prices) {
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const collection = db.collection("prices");
-        await insertStuff(collection, prices);
-        client.close();
-        return Promise.resolve();
-    } catch (error) {
-        return Promise.reject(error);
-    }
+  return result;
+};
+async function priceUpdater(geckoIds) {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection("prices");
+    const prices = await getPriceData(geckoIds);
+    const res = await insertStuff(collection, prices);
+    console.log(res);
+    client.close();
+  } catch (error) {
+    console.log("Error while updating DB:", error);
+  }
 }
 
-module.exports = getPriceData;
+module.exports = priceUpdater;
