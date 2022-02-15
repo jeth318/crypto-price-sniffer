89f7739b52c5a781ea02a473c9bca99d20ed075c
jeth318 <jesper.thornberg@me.com>
2022-02-16 00:47:48 +0100

    add more jenv vars

--- jest.vars.js
+++ jest.vars.js
@@ -1,2 +1,7 @@
 process.env.BINANCE_API_KEY = 'key';
 process.env.BINANCE_API_SECRET = 'secret';
+process.env.MONGODB_ENDPOINT = 'endpoint';
+process.env.DB_PASSWORD = 'password';
+process.env.DB_USERNAME = 'username';
+process.env.DB_NAME = 'db-name';
+process.env.POLL_INTERVAL = 60000;
