9cf74a5b49004a5f058eba07cae51a415dd93e70
jeth318 <jesper.thornberg@me.com>
2022-12-04 23:29:24 +0100

    Remove console log

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import { cwd } from 'process';
-console.log(('CWD', path.join(cwd(), '.env')));
 dotenv.config({ path: path.join(cwd(), '.env') });
